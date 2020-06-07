ce219e99a018946fee491d1d2619f03992358306
YLzhuyanlin <zhuyanlinyl@icloud.com>
2020-06-07 10:37:52 +0800

    treasure hunt

---
+++ learn-to-code-1/2.7-treasure-hunt.swift
@@ -0,0 +1,30 @@
+func turnAround() {
+    turnLeft()
+    turnLeft()
+}
+func moveAndToggle() {
+    moveForward()
+    moveForward()
+    toggleSwitch()
+}
+
+moveAndToggle()
+turnAround()
+moveForward()
+moveForward()
+moveAndToggle()
+turnAround()
+moveForward()
+moveForward()
+
+turnRight()
+
+moveAndToggle()
+moveAndToggle()
+turnAround()
+moveForward()
+moveForward()
+moveForward()
+moveForward()
+moveAndToggle()
+moveAndToggle()
