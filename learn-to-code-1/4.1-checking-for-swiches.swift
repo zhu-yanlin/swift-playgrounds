71b92d3e7e936f2566219385d1a8dc07850804ab
YLzhuyanlin <zhuyanlinyl@icloud.com>
2020-06-12 21:56:13 +0800

    add solution 2 for checking for switches

--- learn-to-code-1/4.1-checking-for-swiches.swift
+++ learn-to-code-1/4.1-checking-for-swiches.swift
@@ -7,3 +7,19 @@ func move(distance: Int) {
     }
 }
 move(distance: 4)
+
+// Solution 2 
+func checkToggle() {
+    if isOnClosedSwitch {
+        toggleSwitch()
+    }
+}
+
+func move(distance: Int) {
+    for i in 1 ... distance {
+        moveForward()
+        checkToggle()
+    }
+}
+
+move(distance: 4)
