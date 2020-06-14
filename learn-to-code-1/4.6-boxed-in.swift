ad147dfe29872110c2d4bf6c1c930590da98a831
YLzhuyanlin <zhuyanlinyl@icloud.com>
2020-06-14 17:59:38 +0800

    boxed in

---
+++ learn-to-code-1/4.6-boxed-in.swift
@@ -0,0 +1,20 @@
+
+func move(step: Int){
+    for i in 1 ... step {
+        moveForward()
+        if isOnClosedSwitch {
+            toggleSwitch()
+        }
+        if isOnGem{
+            collectGem()
+        }
+    }
+}
+moveForward()
+turnLeft()
+moveForward()
+for i in  1 ... 4 {
+    turnLeft()
+    move(step: 2)
+}
+
